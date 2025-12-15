1bf8771c9de192f6f909a47803266767db918a6c
kyle <kyle@chitmeo.com>
2025-12-15 11:58:25 +0700

    Handle Server Errors

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 
-import getContacts  from '@salesforce/apex/ContactController.getContacts'
+import getContacts from '@salesforce/apex/ContactController.getContacts'
 
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
@@ -13,8 +14,15 @@ const COLUMNS = [
 ]
 
 
-export default class ContactList extends LightningElement { 
+export default class ContactList extends LightningElement {
     columns = COLUMNS;
+
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return this.contacts.error ? 
+             reduceErrors(this.contacts.error) : [];
+    }
+
 }
